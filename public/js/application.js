e28f8332485aa7346056b8ecbb44278eb13effc9
kevcorper <kevcorper@gmail.com>
2016-05-05 21:57:46 -0400

    update jquery

--- public/js/application.js
+++ public/js/application.js
@@ -1,20 +1,20 @@
 $(document).ready(function() {
-	$('.create-egg').on('click', function(event) {
-		event.preventDefault();
-		$target = $(event.target)
+	// $('.create-egg').on('click', function(event) {
+	// 	event.preventDefault();
+	// 	$target = $(event.target)
 
-		if ($target.text() == "hide form") {
-			$target.parent().find('.new-post').remove();
-			$target.text('create egg');
-		}
-		else {
-			$.ajax({url: $target.attr("href")})
-				.done(function(response) {
-					$target.after(response);
-					$target.text("hide form");
-				})
-		};
-	});
+	// 	if ($target.text() == "hide form") {
+	// 		$target.parent().find('.new-post').remove();
+	// 		$target.text('create egg');
+	// 	}
+	// 	else {
+	// 		$.ajax({url: $target.attr("href")})
+	// 			.done(function(response) {
+	// 				$target.after(response);
+	// 				$target.text("hide form");
+	// 			})
+	// 	};
+	// });
 	
 
 	$('.comments-button').on('click', function() {
